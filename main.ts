6cf5480fee1d1b7cb3b853341f8cf2d0bccdc140
pablosanchezduro-ux <pablosanchezduro@gmail.com>
2025-11-27 10:21:07 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    music.ringTone(523)
+    music.ringTone(523)
+    music.ringTone(587)
+    music.ringTone(659)
+    music.ringTone(659)
+    music.ringTone(587)
+    music.ringTone(523)
+})
+basic.showIcon(IconNames.Confused)
+basic.forever(function () {
+	
+})
